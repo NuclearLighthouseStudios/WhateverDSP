44fdb7ae921641190b5e1523057072e626ab5cd3
Kris Slyka <kris@slyka.net>
2021-04-28 13:29:55 +0200

    Add flash target to board makefile

--- boards/wdsp-dev/board.mk
+++ boards/wdsp-dev/board.mk
@@ -5,3 +5,7 @@ vscode:
 	cp -r $(WDSP_PATH)/boards/$(BOARD)/vscode/ .vscode
 	sed -i 's#_WDSP_PATH_#$(WDSP_PATH)#' .vscode/c_cpp_properties.json
 	sed -i 's#_TARGET_#$(TARGET)#' .vscode/launch.json
+
+.PHONY: flash
+flash: $(TARGET).hex
+	st-flash --opt --reset --connect-under-reset --format ihex write $(TARGET).hex
