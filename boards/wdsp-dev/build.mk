c85dd0156a8c077dbb7a01b14fb2e69a9e7dc29d
Kris Slyka <kris@slyka.net>
2021-04-28 11:58:08 +0200

    make midi phy configurable through header

--- boards/wdsp-dev/build.mk
+++ boards/wdsp-dev/build.mk
@@ -1,8 +1,11 @@
 # C sources
-C_SOURCES +=  \
+C_SOURCES += \
 src/midi.c \
 boards/$(BOARD)/src/main.c \
 cores/$(CORE)/src/system.c \
 cores/$(CORE)/src/io.c\
 cores/$(CORE)/src/audio.c\
 cores/$(CORE)/src/midi_uart.c
+
+C_INCLUDES += \
+-I boards/$(BOARD)/conf
--- cores/stm32f405rgt/src/midi_uart.c
+++ cores/stm32f405rgt/src/midi_uart.c
@@ -4,78 +4,71 @@
 
 #include "midi.h"
 
+#include "midi_uart.conf.h"
+
 void midi_phy_transmit(unsigned char *data, unsigned short int length)
 {
 	// Set base addresses
-	DMA2_Stream7->M0AR = (uint32_t)(data);
-	DMA2_Stream7->PAR = (uint32_t)(&(USART1->DR));
-	DMA2_Stream7->NDTR = (uint16_t)(length);
+	MIDI_DMA_STREAM->M0AR = (uint32_t)(data);
+	MIDI_DMA_STREAM->PAR = (uint32_t)(&(MIDI_UART->DR));
+	MIDI_DMA_STREAM->NDTR = (uint16_t)(length);
 
 	// Clear DMA transfer complete flag
-	SET_BIT(DMA2->HIFCR, DMA_HIFCR_CTCIF7);
+	SET_BIT(MIDI_DMA->MIDI_DMA_TC_FLAG_REG, MIDI_DMA_TC_FLAG);
 
 	// Enable DMA channel
-	SET_BIT(DMA2_Stream7->CR, DMA_SxCR_EN);
+	SET_BIT(MIDI_DMA_STREAM->CR, DMA_SxCR_EN);
 }
 
 bool midi_phy_can_transmit(void)
 {
-	return !READ_BIT(DMA2_Stream7->CR, DMA_SxCR_EN);
+	return !READ_BIT(MIDI_DMA_STREAM->CR, DMA_SxCR_EN);
 }
 
-void USART1_IRQHandler(void)
+void MIDI_UART_IRQ_HANDLER(void)
 {
-	if (READ_BIT(USART1->SR, USART_SR_RXNE))
+	if (READ_BIT(MIDI_UART->SR, USART_SR_RXNE))
 	{
-		CLEAR_BIT(USART1->SR, USART_SR_RXNE);
-		CLEAR_BIT(USART1->SR, USART_SR_ORE);
+		CLEAR_BIT(MIDI_UART->SR, USART_SR_RXNE);
+		CLEAR_BIT(MIDI_UART->SR, USART_SR_ORE);
 
-		midi_receive(USART1->DR);
+		midi_receive(MIDI_UART->DR);
 	}
 
-	if (READ_BIT(USART1->SR, USART_SR_TC))
+	if (READ_BIT(MIDI_UART->SR, USART_SR_TC))
 	{
-		CLEAR_BIT(USART1->SR, USART_SR_TC);
+		CLEAR_BIT(MIDI_UART->SR, USART_SR_TC);
 		midi_transmit();
 	}
 }
 
 void midi_phy_init(void)
 {
-	// Set up pins
-	MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODER7_Msk, 0b10 << GPIO_MODER_MODER7_Pos);
-	MODIFY_REG(GPIOB->PUPDR, GPIO_PUPDR_PUPD7_Msk, 0b01 << GPIO_PUPDR_PUPD7_Pos);
-	MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL7_Msk, 7 << GPIO_AFRL_AFSEL7_Pos);
-
-	MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODER6_Msk, 0b10 << GPIO_MODER_MODER6_Pos);
-	MODIFY_REG(GPIOB->AFR[0], GPIO_AFRL_AFSEL6_Msk, 7 << GPIO_AFRL_AFSEL6_Pos);
-
-	// Enable USART1 periphery
-	SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART1EN);
+	midi_phy_setup();
 
 	// Set baud rate (31250 bps)
-	MODIFY_REG(USART1->BRR, USART_BRR_DIV_Mantissa_Msk, 168ul << USART_BRR_DIV_Mantissa_Pos);
+	MODIFY_REG(MIDI_UART->BRR, USART_BRR_DIV_Mantissa_Msk, 168ul << USART_BRR_DIV_Mantissa_Pos);
 
-	SET_BIT(USART1->CR1, USART_CR1_RE);		// Enable receiver
-	SET_BIT(USART1->CR1, USART_CR1_RXNEIE); // Enable receive interrupt
+	SET_BIT(MIDI_UART->CR1, USART_CR1_RE);		// Enable receiver
+	SET_BIT(MIDI_UART->CR1, USART_CR1_RXNEIE); // Enable receive interrupt
 
 	// Set up transmit DMA
-	MODIFY_REG(DMA2_Stream7->CR, DMA_SxCR_PL_Msk, 0b00 << DMA_SxCR_PL_Pos);		 // Lowest Priority
-	MODIFY_REG(DMA2_Stream7->CR, DMA_SxCR_CHSEL_Msk, 0x4 << DMA_SxCR_CHSEL_Pos); // Channel 4 for USART1 TX
+	MODIFY_REG(MIDI_DMA_STREAM->CR, DMA_SxCR_PL_Msk, 0b00 << DMA_SxCR_PL_Pos);		 // Lowest Priority
+	MODIFY_REG(MIDI_DMA_STREAM->CR, DMA_SxCR_CHSEL_Msk, 0x4 << DMA_SxCR_CHSEL_Pos); // Channel 4 for USART1 TX
 
 	// 8 bit to 8 bit transfers
-	MODIFY_REG(DMA2_Stream7->CR, DMA_SxCR_MSIZE_Msk, 0b00 << DMA_SxCR_MSIZE_Pos);
-	MODIFY_REG(DMA2_Stream7->CR, DMA_SxCR_PSIZE_Msk, 0b00 << DMA_SxCR_PSIZE_Pos);
+	MODIFY_REG(MIDI_DMA_STREAM->CR, DMA_SxCR_MSIZE_Msk, 0b00 << DMA_SxCR_MSIZE_Pos);
+	MODIFY_REG(MIDI_DMA_STREAM->CR, DMA_SxCR_PSIZE_Msk, 0b00 << DMA_SxCR_PSIZE_Pos);
 
 	// Memory to peripheral with memory increment
-	MODIFY_REG(DMA2_Stream7->CR, DMA_SxCR_DIR_Msk, 0b01 << DMA_SxCR_DIR_Pos);
-	SET_BIT(DMA2_Stream7->CR, DMA_SxCR_MINC);
+	MODIFY_REG(MIDI_DMA_STREAM->CR, DMA_SxCR_DIR_Msk, 0b01 << DMA_SxCR_DIR_Pos);
+	SET_BIT(MIDI_DMA_STREAM->CR, DMA_SxCR_MINC);
 
-	SET_BIT(USART1->CR1, USART_CR1_TE);	  // Enable transmitter
-	SET_BIT(USART1->CR1, USART_CR1_TCIE); // Enable transmit complete interrupt
-	SET_BIT(USART1->CR3, USART_CR3_DMAT); // Enable transmit DMA
+	SET_BIT(MIDI_UART->CR1, USART_CR1_TE);	  // Enable transmitter
+	SET_BIT(MIDI_UART->CR1, USART_CR1_TCIE); // Enable transmit complete interrupt
+	SET_BIT(MIDI_UART->CR3, USART_CR3_DMAT); // Enable transmit DMA
 
-	NVIC_EnableIRQ(USART1_IRQn);
+	NVIC_EnableIRQ(MIDI_UART_IRQ);
 
-	SET_BIT(USART1->CR1, USART_CR1_UE); // Enable UART
+	SET_BIT(MIDI_UART->CR1, USART_CR1_UE); // Enable UART
 }
