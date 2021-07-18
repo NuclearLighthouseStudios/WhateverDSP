2ed5350fef349c111bbeb90193cc3f6ee08d8be3
Kris Slyka <kris@slyka.net>
2021-07-18 13:03:40 +0200

    Clean up and refactoring

--- cores/stm32f405rgt/src/usb_phy.c
+++ cores/stm32f405rgt/src/usb_phy.c
@@ -30,7 +30,7 @@ static int __CCMRAM fifo_alloc_pos;
 static int __CCMRAM fifo_alloc_num;
 
 
-static int usb_phy_fifo_alloc(size_t size)
+static int fifo_alloc(size_t size)
 {
 	if (fifo_alloc_num == 0)
 	{
@@ -50,7 +50,7 @@ static int usb_phy_fifo_alloc(size_t size)
 }
 
 
-static void usb_phy_fifo_read(uint8_t *buf, size_t size)
+static void fifo_read(uint8_t *buf, size_t size)
 {
 	while (size > 0)
 	{
@@ -72,7 +72,7 @@ static void usb_phy_fifo_read(uint8_t *buf, size_t size)
 	}
 }
 
-static void usb_phy_fifo_write(uint8_t *buf, size_t size, int fifo_num)
+static void fifo_write(uint8_t *buf, size_t size, int fifo_num)
 {
 	while (size > 0)
 	{
@@ -89,6 +89,167 @@ static void usb_phy_fifo_write(uint8_t *buf, size_t size, int fifo_num)
 		}
 
 		USB_OTG_FS_DFIFO(fifo_num) = data;
+	}
+}
+
+void OTG_FS_IRQHandler(void)
+{
+	if (READ_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_USBRST))
+	{
+		SET_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_USBRST);
+		usb_reset();
+	}
+
+	if (READ_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_RXFLVL))
+	{
+		uint32_t status = USB_OTG_FS->GRXSTSP;
+
+		int epnum = status & USB_OTG_GRXSTSP_EPNUM;
+		int size = (status & USB_OTG_GRXSTSP_BCNT_Msk) >> USB_OTG_GRXSTSP_BCNT_Pos;
+		int type = (status & USB_OTG_GRXSTSP_PKTSTS_Msk) >> USB_OTG_GRXSTSP_PKTSTS_Pos;
+
+		usb_out_endpoint *ep = &(usb_out_eps[epnum]);
+
+		if (size > 0)
+		{
+			switch (type)
+			{
+				case 0b0010: // Data packet
+				{
+					// If reading the data would overflow the rx buffer, read only as much as we can and discard the rest
+					if (ep->rx_count + size > ep->rx_size)
+					{
+						fifo_read((uint8_t *)ep->rx_buffer + ep->rx_count, ep->rx_size - ep->rx_count);
+						fifo_read(NULL, ep->rx_count + size - ep->rx_size);
+						ep->rx_count = ep->rx_size;
+					}
+					else
+					{
+						fifo_read((uint8_t *)ep->rx_buffer + ep->rx_count, size);
+						ep->rx_count += size;
+					}
+					break;
+				}
+
+				case 0b0110: // Setup packet
+				{
+					fifo_read((uint8_t *)&(ep->setup_packet), 8);
+					break;
+				}
+			}
+		}
+	}
+
+	// OUT endpoint interrupts (receive)
+	if (READ_BIT(USB_OTG_FS_DEVICE->DAINT, USB_OTG_DAINT_OEPINT))
+	{
+		uint32_t ep_int_flags = (USB_OTG_FS_DEVICE->DAINT & USB_OTG_DAINT_OEPINT) >> USB_OTG_DAINT_OEPINT_Pos;
+		int epnum = 0;
+
+		while (ep_int_flags)
+		{
+			if (ep_int_flags & 0b1)
+			{
+				uint32_t ints = USB_OTG_FS_OUTEP(epnum)->DOEPINT;
+				ints &= USB_OTG_FS_DEVICE->DOEPMSK;
+
+				usb_out_endpoint *ep = &(usb_out_eps[epnum]);
+
+				// Setup ready
+				if (READ_BIT(ints, USB_OTG_DOEPINT_STUP))
+				{
+					ep->setup_ready = true;
+					MODIFY_REG(USB_OTG_FS_OUTEP(epnum)->DOEPTSIZ, USB_OTG_DOEPTSIZ_STUPCNT_Msk, 3 << USB_OTG_DOEPTSIZ_STUPCNT_Pos);
+					SET_BIT(USB_OTG_FS_OUTEP(epnum)->DOEPINT, USB_OTG_DOEPINT_STUP);
+				}
+
+				// Receive transfer complete
+				if (READ_BIT(ints, USB_OTG_DOEPINT_XFRC))
+				{
+					if (ep->rx_count < ep->rx_size)
+					{
+						size_t left = (USB_OTG_FS_OUTEP(epnum)->DOEPTSIZ & USB_OTG_DOEPTSIZ_XFRSIZ_Msk) >> USB_OTG_DOEPTSIZ_XFRSIZ_Pos;
+
+						// If the current transfer went through completely start another one.
+						// Transfers should always end with a short or zero length packet.
+						if (left == 0)
+							usb_phy_receive(ep);
+						else
+							ep->rx_ready = true;
+					}
+					else
+					{
+						ep->rx_ready = true;
+					}
+
+					SET_BIT(USB_OTG_FS_OUTEP(epnum)->DOEPINT, USB_OTG_DOEPINT_XFRC);
+				}
+			}
+
+			epnum++;
+			ep_int_flags >>= 1;
+		}
+	}
+
+	// IN endpoint interrupts (transmit)
+	if (READ_BIT(USB_OTG_FS_DEVICE->DAINT, USB_OTG_DAINT_IEPINT))
+	{
+		uint32_t ep_int_flags = (USB_OTG_FS_DEVICE->DAINT & USB_OTG_DAINT_IEPINT) >> USB_OTG_DAINT_IEPINT_Pos;
+		int epnum = 0;
+
+		while (ep_int_flags)
+		{
+			if (ep_int_flags & 0b1)
+			{
+				uint32_t ints = USB_OTG_FS_INEP(epnum)->DIEPINT;
+				ints &= USB_OTG_FS_DEVICE->DIEPMSK | USB_OTG_DIEPINT_TXFE;
+
+				usb_in_endpoint *ep = &(usb_in_eps[epnum]);
+
+				// Transmit fifo empty
+				if ((READ_BIT(ints, USB_OTG_DIEPINT_TXFE)) && (READ_BIT(USB_OTG_FS_DEVICE->DIEPEMPMSK, 0b1 << epnum)))
+				{
+					size_t empty = USB_OTG_FS_INEP(epnum)->DTXFSTS * 4;
+					size_t count = (USB_OTG_FS_INEP(ep->epnum)->DIEPTSIZ & USB_OTG_DIEPTSIZ_XFRSIZ_Msk) >> USB_OTG_DIEPTSIZ_XFRSIZ_Pos;
+
+					if (count > empty) count = empty;
+
+					fifo_write(ep->tx_buffer + ep->tx_count, count, ep->fifo_num);
+					ep->tx_count += count;
+
+					if (ep->tx_count >= ep->tx_size)
+						CLEAR_BIT(USB_OTG_FS_DEVICE->DIEPEMPMSK, 0b1 << epnum);
+				}
+
+				// Transmit transfer complete
+				if (READ_BIT(ints, USB_OTG_DIEPINT_XFRC))
+				{
+					if (ep->tx_count >= ep->tx_size)
+					{
+						// In case our amount of data to send doesn't result in a "short packet" at the end
+						// we need to send one more zero size packet to signal the end of transmission
+						if ((ep->tx_size != 0) && (ep->tx_size % ep->max_packet_size == 0))
+						{
+							ep->tx_size = 0;
+							usb_phy_transmit(ep);
+						}
+						else
+						{
+							ep->tx_ready = true;
+						}
+					}
+					else
+					{
+						usb_phy_transmit(ep);
+					}
+
+					SET_BIT(USB_OTG_FS_INEP(epnum)->DIEPINT, USB_OTG_DIEPINT_XFRC);
+				}
+			}
+
+			epnum++;
+			ep_int_flags >>= 1;
+		}
 	}
 }
 
@@ -120,9 +281,7 @@ void usb_phy_transmit(usb_in_endpoint *ep)
 	SET_BIT(USB_OTG_FS_INEP(ep->epnum)->DIEPCTL, USB_OTG_DIEPCTL_EPENA | USB_OTG_DIEPCTL_CNAK);
 
 	if (size > 0)
-	{
 		SET_BIT(USB_OTG_FS_DEVICE->DIEPEMPMSK, 0b1 << ep->epnum);
-	}
 }
 
 void usb_phy_receive(usb_out_endpoint *ep)
@@ -171,9 +330,16 @@ void usb_phy_in_ep_init(usb_in_endpoint *ep)
 
 	SET_BIT(USB_OTG_FS_INEP(epnum)->DIEPCTL, USB_OTG_DIEPCTL_SNAK | USB_OTG_DIEPCTL_USBAEP);
 
-	int fifo_num = usb_phy_fifo_alloc(ep->fifo_size);
+	int fifo_num = fifo_alloc(ep->fifo_size);
 	ep->fifo_num = fifo_num;
 	MODIFY_REG(USB_OTG_FS_INEP(epnum)->DIEPCTL, USB_OTG_DIEPCTL_TXFNUM_Msk, fifo_num << USB_OTG_DIEPCTL_TXFNUM_Pos);
+
+	// Flush transmit fifo
+	SET_BIT(USB_OTG_FS->GRSTCTL, epnum << USB_OTG_GRSTCTL_TXFNUM_Pos);
+	SET_BIT(USB_OTG_FS->GRSTCTL, USB_OTG_GRSTCTL_TXFFLSH);
+
+	while (READ_BIT(USB_OTG_FS->GRSTCTL, USB_OTG_GRSTCTL_TXFFLSH))
+		__NOP();
 
 	if (epnum == 0)
 	{
@@ -219,166 +385,9 @@ void usb_phy_out_ep_init(usb_out_endpoint *ep)
 }
 
 
-void OTG_FS_IRQHandler(void)
+void usb_phy_set_address(int address)
 {
-	if (READ_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_USBRST))
-	{
-		SET_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_USBRST);
-		usb_reset();
-	}
-
-	if (READ_BIT(USB_OTG_FS->GINTSTS, USB_OTG_GINTSTS_RXFLVL))
-	{
-		CLEAR_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTSTS_RXFLVL);
-
-		uint32_t status = USB_OTG_FS->GRXSTSP;
-
-		int epnum = status & USB_OTG_GRXSTSP_EPNUM;
-		int size = (status & USB_OTG_GRXSTSP_BCNT_Msk) >> USB_OTG_GRXSTSP_BCNT_Pos;
-		int type = (status & USB_OTG_GRXSTSP_PKTSTS_Msk) >> USB_OTG_GRXSTSP_PKTSTS_Pos;
-
-		usb_out_endpoint *ep = &(usb_out_eps[epnum]);
-
-		if (size > 0)
-		{
-			switch (type)
-			{
-				case 0b0010: // Data packet
-					// If reading the data would overflow the rx buffer, read only as much as we can and discard the rest
-					if (ep->rx_count + size > ep->rx_size)
-					{
-						usb_phy_fifo_read((uint8_t *)ep->rx_buffer + ep->rx_count, ep->rx_size - ep->rx_count);
-						usb_phy_fifo_read(NULL, ep->rx_count + size - ep->rx_size);
-						ep->rx_count = ep->rx_size;
-					}
-					else
-					{
-						usb_phy_fifo_read((uint8_t *)ep->rx_buffer + ep->rx_count, size);
-						ep->rx_count += size;
-					}
-					break;
-
-				case 0b0110: // Setup packet
-					usb_phy_fifo_read((uint8_t *)&(ep->setup_packet), 8);
-					break;
-
-				default:
-					printf("EP:%d, S:%d, T:0x%02x\n", epnum, size, type);
-					break;
-			}
-		}
-
-		SET_BIT(USB_OTG_FS->GINTMSK, USB_OTG_GINTSTS_RXFLVL);
-	}
-
-	// OUT endpoint interrupts (receive)
-	if (READ_BIT(USB_OTG_FS_DEVICE->DAINT, USB_OTG_DAINT_OEPINT))
-	{
-		uint32_t eps = (USB_OTG_FS_DEVICE->DAINT & USB_OTG_DAINT_OEPINT) >> USB_OTG_DAINT_OEPINT_Pos;
-		int epnum = 0;
-
-		while (eps)
-		{
-			if (eps & 0b1)
-			{
-				uint32_t ints = USB_OTG_FS_OUTEP(epnum)->DOEPINT;
-				ints &= USB_OTG_FS_DEVICE->DOEPMSK;
-
-				usb_out_endpoint *ep = &(usb_out_eps[epnum]);
-
-				if (READ_BIT(ints, USB_OTG_DOEPINT_STUP))
-				{
-					ep->setup_ready = true;
-					MODIFY_REG(USB_OTG_FS_OUTEP(epnum)->DOEPTSIZ, USB_OTG_DOEPTSIZ_STUPCNT_Msk, 3 << USB_OTG_DOEPTSIZ_STUPCNT_Pos);
-					SET_BIT(USB_OTG_FS_OUTEP(epnum)->DOEPINT, USB_OTG_DOEPINT_STUP);
-				}
-
-				if (READ_BIT(ints, USB_OTG_DOEPINT_XFRC))
-				{
-					if (ep->rx_count < ep->rx_size)
-					{
-						size_t left = (USB_OTG_FS_OUTEP(ep->epnum)->DOEPTSIZ & USB_OTG_DOEPTSIZ_XFRSIZ_Msk) >> USB_OTG_DOEPTSIZ_XFRSIZ_Pos;
-
-						// If the current transfer went through completely start another one
-						// transfers should always end with a short or sezo length packet
-						// BUT: this does not seem to be the case with test packets I send using pyusb?!
-						if (left == 0)
-							usb_phy_receive(ep);
-						else
-							ep->rx_ready = true;
-					}
-					else
-					{
-						ep->rx_ready = true;
-					}
-
-					SET_BIT(USB_OTG_FS_OUTEP(epnum)->DOEPINT, USB_OTG_DOEPINT_XFRC);
-				}
-			}
-
-			epnum++;
-			eps >>= 1;
-		}
-	}
-
-	// IN endpoint interrupts (transmit)
-	if (READ_BIT(USB_OTG_FS_DEVICE->DAINT, USB_OTG_DAINT_IEPINT))
-	{
-		uint32_t eps = (USB_OTG_FS_DEVICE->DAINT & USB_OTG_DAINT_IEPINT) >> USB_OTG_DAINT_IEPINT_Pos;
-		int epnum = 0;
-
-		while (eps)
-		{
-			if (eps & 0b1)
-			{
-				uint32_t ints = USB_OTG_FS_INEP(epnum)->DIEPINT;
-				ints &= USB_OTG_FS_DEVICE->DIEPMSK | USB_OTG_DIEPINT_TXFE;
-
-				usb_in_endpoint *ep = &(usb_in_eps[epnum]);
-
-				if ((READ_BIT(ints, USB_OTG_DIEPINT_TXFE)) && (READ_BIT(USB_OTG_FS_DEVICE->DIEPEMPMSK, 0b1 << epnum)))
-				{
-					size_t empty = USB_OTG_FS_INEP(epnum)->DTXFSTS * 4;
-					size_t count = (USB_OTG_FS_INEP(ep->epnum)->DIEPTSIZ & USB_OTG_DIEPTSIZ_XFRSIZ_Msk) >> USB_OTG_DIEPTSIZ_XFRSIZ_Pos;
-
-					if (count > empty) count = empty;
-
-					usb_phy_fifo_write(ep->tx_buffer + ep->tx_count, count, ep->fifo_num);
-					ep->tx_count += count;
-
-					if (ep->tx_count >= ep->tx_size)
-						CLEAR_BIT(USB_OTG_FS_DEVICE->DIEPEMPMSK, 0b1 << epnum);
-				}
-
-				if (READ_BIT(ints, USB_OTG_DIEPINT_XFRC))
-				{
-					if (ep->tx_count >= ep->tx_size)
-					{
-						// In case our amount of data to send doesn't result in a "short packet" at the end
-						// we need to send one more zero size packet to signal the end of transmission
-						if ((ep->tx_size != 0) && (ep->tx_size % ep->max_packet_size == 0))
-						{
-							ep->tx_size = 0;
-							usb_phy_transmit(ep);
-						}
-						else
-						{
-							ep->tx_ready = true;
-						}
-					}
-					else
-					{
-						usb_phy_transmit(ep);
-					}
-
-					SET_BIT(USB_OTG_FS_INEP(epnum)->DIEPINT, USB_OTG_DIEPINT_XFRC);
-				}
-			}
-
-			epnum++;
-			eps >>= 1;
-		}
-	}
+	MODIFY_REG(USB_OTG_FS_DEVICE->DCFG, USB_OTG_DCFG_DAD_Msk, (address & 0x7f) << USB_OTG_DCFG_DAD_Pos);
 }
 
 
@@ -424,13 +433,6 @@ void usb_phy_flush_fifos(void)
 	while (READ_BIT(USB_OTG_FS->GRSTCTL, USB_OTG_GRSTCTL_RXFFLSH))
 		__NOP();
 }
-
-
-void usb_phy_set_address(int address)
-{
-	MODIFY_REG(USB_OTG_FS_DEVICE->DCFG, USB_OTG_DCFG_DAD_Msk, (address & 0x7f) << USB_OTG_DCFG_DAD_Pos);
-}
-
 
 void usb_phy_init(usb_in_endpoint in_eps[], usb_out_endpoint out_eps[])
 {
