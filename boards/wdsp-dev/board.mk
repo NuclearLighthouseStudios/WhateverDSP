acbba8f015221750a612c7dc5635721377bf8f68
miunau <miunau@miunau.com>
2021-08-08 11:32:21 +0200

    added make skel to initialise a project skeleton from Makefile target

--- boards/wdsp-dev/board.mk
+++ boards/wdsp-dev/board.mk
@@ -10,4 +10,9 @@ vscode:
 	sed -i'' -e 's#_WDSP_PATH_#$(WDSP_PATH)#;s#_BOARD_#$(BOARD)#;s#_CORE_#$(CORE)#' .vscode/c_cpp_properties.json
 	sed -i'' -e 's#_TARGET_#$(TARGET)#' .vscode/launch.json
 
+.PHONY: skel
+skel:
+	cp $(WDSP_PATH)/boards/$(BOARD)/skel/skel.c ${TARGET}.c
+	cp $(WDSP_PATH)/boards/$(BOARD)/config.ini .
+
 endif
